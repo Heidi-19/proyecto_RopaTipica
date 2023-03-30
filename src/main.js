c0aa80b86f91d4be928b7740c5254285775aba12
Heidi Lucy Bautista Sanjuan <71859829+Heidi-19@users.noreply.github.com>
2023-03-30 10:23:11 -0600

    Perfil

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import sesion from './sesion.vue'
+import sesion from './perfil.vue'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
